771afc053f6f19eba81ace90deea4bbd85ea7d0a
Huan Zhang <ecezhang@ucdavis.edu>
2018-07-18 04:09:09 -0400

    add alexnet

--- run_networks.sh
+++ run_networks.sh
@@ -5,8 +5,9 @@
 rm /tmp/gpu_lock_*
 
 model="$@"
+nthreads=6
 
-declare -A batchsize=( ["resnet_v2_50"]="128" ["resnet_v2_101"]="128" ["resnet_v2_152"]="64" ["inception_v1"]="512" ["inception_v2"]="512" ["inception_v3"]="256" ["inception_v4"]="64" ["inception_resnet_v2"]="64" ["vgg_16"]="128" ["vgg_19"]="128" ["mobilenet_v1_025"]="1024" ["mobilenet_v1_050"]="1024" ["mobilenet_v1_100"]="512" ["nasnet_large"]="16" ["densenet121_k32"]="128" ["densenet169_k32"]="128" ["densenet161_k48"]="64" )
+declare -A batchsize=( ["resnet_v2_50"]="128" ["resnet_v2_101"]="128" ["resnet_v2_152"]="64" ["inception_v1"]="512" ["inception_v2"]="512" ["inception_v3"]="256" ["inception_v4"]="64" ["inception_resnet_v2"]="64" ["vgg_16"]="128" ["vgg_19"]="128" ["mobilenet_v1_025"]="1024" ["mobilenet_v1_050"]="1024" ["mobilenet_v1_100"]="512" ["nasnet_large"]="16" ["densenet121_k32"]="128" ["densenet169_k32"]="128" ["densenet161_k48"]="64" ["alexnet"]="512")
 
 for m in $model; do
     if [ ! ${batchsize[$m]+abc} ]; then
@@ -19,7 +20,7 @@ for m in $model; do
     for f in 0 25 50 75; do
         b="${batchsize[$m]}"
         output_dir="results/${m}/${m}_${f}"
-        echo python3 collect_gradients.py -d imagenet -m ${m} -f ${f} --nthreads 6 --ids target.tsv -n 25 -s ${output_dir} --batch_size $b
+        echo python3 collect_gradients.py -d imagenet -m ${m} -f ${f} --nthreads ${nthreads} --ids target.tsv -n 25 -s ${output_dir} --batch_size $b
     done
 done
 
